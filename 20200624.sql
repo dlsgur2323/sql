42fd85573f16b05d2307c134cb204cdd47dcc833
dlsgur <dlsgur2323@gmail.com>
2020-06-25 10:10:56 +0900

    202000624

---
+++ 20200624.sql
@@ -0,0 +1,16 @@
+SELECT 실습 과제
+
+SELECT *
+FROM prod;
+
+SELECT prod_id, prod_name
+FROM prod;
+
+SELECT buyer_id, buyer_name
+FROM buyer;
+
+SELECT *
+FROM cart;
+
+SELECT mem_id, mem_pass, mem_name
+FROM member;
